5f6a9522d64f23de575245d65aa343515dec370b
Xaverius Yehuda Revaldo Erdi Nugraha <xyehuda3@gmail.com>
2025-05-15 21:23:35 +0700

    perubaham konfigurasi vite

--- front-end/vite.config.mjs
+++ front-end/vite.config.mjs
@@ -2,13 +2,10 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-<<<<<<< HEAD
-=======
   server: {
     host: '0.0.0.0',
     port: 8080
   },
->>>>>>> 138c4ae (sss)
   plugins: [
     tailwindcss(),
   ],
@@ -16,11 +13,5 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-<<<<<<< HEAD
-  },
-  server: {
-    port: 5173, // default Vite port
-=======
->>>>>>> 138c4ae (sss)
   }
 });
